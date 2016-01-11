c8fdcc31ba45b9e0d6b7e716a196d306c320d17e
Olli Korhonen <olli.korhonen@iki.fi>
2016-01-11 19:20:24 +0200

    storage transfer added

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+app.get('/transfer', function(request, response) {
+  response.render('pages/transfer');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
