eff8125f5f0829ce87db0181beddb2438fc2259a
Olli Korhonen <olli.korhonen@iki.fi>
2016-01-15 11:54:10 +0200

    added scripts or populating db

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -4,12 +4,54 @@ var products = [
     {"id": 2, "name": "Fizz Päärynä tölkki 0,5l", "qtys": [{"n": "6", q: 6}, {"n": "2x6", q: 12}, {"n": "3x6", q: 18}]}
 ];
 
+// /* Populate pruducts table in db */
+
+//    $.each(products, function (key, i) {
+//     console.log(JSON.stringify(i));
+//         $.ajax({
+//             type: 'PUT',
+//             url: '/api/products',
+//             contentType: 'application/json',
+//             data: JSON.stringify(i)
+//         }).then(function (data) {
+//             console.log('added: ' + data);
+//         }).fail(function (error) {
+//             console.log(error);
+//         });
+//     });
+
 var storages = [
-    {'id': 0, 'name': 'Varasto', 'default': true},
+    {'id': 0, 'name': 'Varasto'},
     {'id': 1, 'name': 'Villipeura'},
     {'id': 2, 'name': 'Villikko'},
     {'id': 3, 'name': 'Villiruusu'}
 ];
+
+// /* Populate storage table in db */
+
+//    $.each(storages, function (key, i) {
+//     console.log(JSON.stringify(i));
+//         $.ajax({
+//             type: 'PUT',
+//             url: '/api/storages',
+//             contentType: 'application/json',
+//             data: JSON.stringify(i)
+//         }).then(function (data) {
+//             console.log('added: ' + data);
+//         }).fail(function (error) {
+//             console.log(error);
+//         });
+//     });
+
+// var storages = [];
+
+// $.getJSON('/api/storages')
+//     .then(function (data) {
+//         storages = data;
+//     })
+//     .fail(function (error) {
+//         console.log(error);
+//     });
 
 function findProduct (id) {
     return _.find(products, { 'id' : id });
@@ -22,16 +64,25 @@ function setQty(qty) {
     $("#qty").val(qty);
 }
 
+//var products = [];
+
 function populateProducts(s) {
     $(s).empty();
-    $.each(products, function (key, p) {
-        s.append($('<option>', {
-            value: p.id,
-            text : p.name
-        }));
-    });
 
-    changeProduct(s);
+   // $.getJSON('/api/products')
+   //     .then(function (data) {
+   //         products = data;
+            $.each(products, function (key, p) {
+                s.append($('<option>', {
+                    value: p.id,
+                    text : p.name
+                }));
+            });
+            changeProduct(s);
+        // })
+        // .fail(function (error) {
+        //     console.log(error);
+        // });
 }
 
 function changeProduct(s) {
