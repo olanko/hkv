bb79661b36e2bf382bcd80327417527e640fac5e
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-06 01:23:27 +0200

    transfer list

--- public/js/transfersctrl.js
+++ public/js/transfersctrl.js
@@ -1,10 +1,13 @@
 angular.module('hkApp.controllers')
 .controller('TransfersCtrl',
-    ['$scope', '$http', 'Storage', 'Transfer',
-    function  ($scope, $http, Storage, Transfer) {
+    ['$scope', '$http', 'Storage', 'Product', 'Transfer',
+    function  ($scope, $http, Storage, Product, Transfer) {
         $scope.transfers = [];
         $scope.storages = [];
+        $scope.products = [];
         $scope.datenow = moment();
+
+        $scope.types = ['Siirto', 'Toimitus', 'Myynti'];
 
         Transfer.find()
             .$promise
@@ -15,7 +18,19 @@ angular.module('hkApp.controllers')
         Storage.find()
             .$promise
             .then(function (data) {
-                $scope.storages = data;
+                /* Make array index match id:s */
+                _(data).forEach(function (i) {
+                    $scope.storages[i.id] = i;
+                });
+            });
+
+        Product.find()
+            .$promise
+            .then(function (data) {
+                /* Make array index match id:s */
+                _(data).forEach(function (i) {
+                    $scope.products[i.id] = i;
+                });
             });
     }
 ]);
