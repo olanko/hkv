1230a0e78f1d0b5e24c4b66f0010384da80edc51
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-06 13:52:38 +0200

    test express-sslify

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('port', (process.env.PORT || 5000));
 
 console.log(process.env);
 
-if (env == 'production') {
+if (env === 'production') {
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
 
