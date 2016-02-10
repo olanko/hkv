de47f0608fb8a7e8822eb76e5720a906ed5d7339
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-10 13:28:37 +0200

    mocks

--- scripts/populate-mock-transfers.sql
+++ scripts/populate-mock-transfers.sql
@@ -2,13 +2,14 @@
 
 
 select * from product;
+select * from storage;
 
 update product set active = true;
 
 truncate table transfer;
 
 insert into transfer (fromstorageid, tostorageid, productid, transfertime, "user", absolute, relative, comment, type)
-	values (null, 0, 0, '2016-01-02 12:00', 0, 168, 0, 'Määrä alussa', 3);
+	values (0, 0, 0, '2016-01-02 12:00', 0, 168, 0, 'Määrä alussa', 3);
 insert into transfer (fromstorageid, tostorageid, productid, transfertime, "user", absolute, relative, comment, type)
 	values (0, 1, 0, '2016-01-02 13:00', 0, 0, 48, '', 0);
 insert into transfer (fromstorageid, tostorageid, productid, transfertime, "user", absolute, relative, comment, type)
@@ -18,7 +19,7 @@ insert into transfer (fromstorageid, tostorageid, productid, transfertime, "user
 insert into transfer (fromstorageid, tostorageid, productid, transfertime, "user", absolute, relative, comment, type)
 	values (0, 1, 0, '2016-01-02 16:00', 0, 0, 24, '', 0);
 insert into transfer (fromstorageid, tostorageid, productid, transfertime, "user", absolute, relative, comment, type)
-	values (1, null, 0, '2016-01-02 17:00', 0, 0, -120, '', 2);
+	values (1, 10, 0, '2016-01-02 17:00', 0, 0, -120, '', 2);
 
 
 
--- scripts/utilities.sql
+++ scripts/utilities.sql
@@ -1,5 +1,9 @@
 ﻿select * from product;
 
+update product set unitqty = 24 where qtys = '[{"n":"24","q":24},{"n":"2x24","q":48},{"n":"3x24","q":72}]';
+update product set unitqty = 6 where qtys = '[{"n":"6","q":6},{"n":"2x6","q":12},{"n":"3x6","q":18}]';
+update product set name = 'Fizz Päärynä tölkki 0,33l' where id = 2;
+
 delete from product where id in (25, 26, 27);
 
 select * from transfer;
@@ -9,6 +13,15 @@ delete from transfer WHERE id in (213, 214, 216);
 select * from storage;
 
 
+insert into storage (name, def, type) values ('Respa', false, 0);
+
 insert into storage (name, def, type) values ('Olvi', false, 1);
 insert into storage (name, def, type) values ('SAT', false, 1);
 insert into storage (name, def, type) values ('Koff', false, 1);
+
+insert into storage (name, def, type) values ('Muu', false, 1);
+
+
+insert into storage (name, def, type) values ('Hävikki', false, 2);
+
+insert into storage (name, def, type) values ('Myynti', false, 3);
