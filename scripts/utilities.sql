391a959b6689de4ef598deb46ed327d56e3728f6
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-10 11:55:50 +0200

    util script

---
+++ scripts/utilities.sql
@@ -0,0 +1,14 @@
+﻿select * from product;
+
+delete from product where id in (25, 26, 27);
+
+select * from transfer;
+
+delete from transfer WHERE id in (213, 214, 216);
+
+select * from storage;
+
+
+insert into storage (name, def, type) values ('Olvi', false, 1);
+insert into storage (name, def, type) values ('SAT', false, 1);
+insert into storage (name, def, type) values ('Koff', false, 1);
