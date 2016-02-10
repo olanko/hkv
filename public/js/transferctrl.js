26a121681506a3c244cba8c7cb5182d6650a6db8
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-10 13:28:21 +0200

    all actions

--- public/js/transferctrl.js
+++ public/js/transferctrl.js
@@ -1,50 +1,125 @@
 angular.module('hkApp.controllers')
-.controller('TransferCtrl', ['$scope', '$http', '$routeParams', 'Product', 'Storage', 'Transfer',
-    function  ($scope, $http, $routeParams, Product, Storage, Transfer) {
-    $scope.newtransfer = {
-        stofrom: {},
-        stodest: {},
-        product: {},
-        relative: 0,
-        absolute: 0,
-        qty: 0
-    };
-
-    $scope.config = {
-        useAbsolute: false
-    };
-
-    $scope.types = ['Siirto', 'Toimitus', 'Myynti', 'Inventaario'];
-    $scope.type = 0;
-    if ($routeParams.type > 0) {
-        $scope.type = $routeParams.type;
-    }
-    console.log($scope.type);
-
+.controller('TransferCtrl',
+    ['$scope', '$http', '$routeParams', '$location', '$q', 'Product', 'Storage', 'Transfer',
+    function  ($scope, $http, $routeParams, $location, $q, Product, Storage, Transfer) {
+    $scope.newtransfer = {};
     $scope.alerts = [];
 
-    Product.find()
-        .$promise
-        .then(function (data) {
-            $scope.products = data;
+    var options = $scope.options = {
+        useAbsolute: false,
+        useNegative: false,
+        fromStorageType: 0,
+        showFromStorage: true,
+        showToStorage: true,
+        showStorageLabels: true
+    };
 
-            _($scope.products).forEach(function (i) {
-                i.qs = JSON.parse(i.qtys);
-            });
+    $scope.actions = {
+        '/transfer': {
+            caption: '',
+            submitCaption: 'Siirrä',
+            type: 0,
+            initOptions: function () {},
+            initData: function () {
+                $scope.newtransfer.stofrom = $scope.newtransfer.stofrom || $scope.storages[0];
+                $scope.newtransfer.stodest = $scope.newtransfer.stodest || $scope.storages[1];
+            },
+            preAdd: function () {},
+            alert: function (n) {
+                return '' + n.qty + ' ' + n.product.name + ' ' + n.stofrom.name + ' -> ' + n.stodest.name;
+            }
+        },
+        '/delivery': {
+            caption: 'Toimitus varastoon',
+            submitCaption: 'Lisää',
+            type: 1,
+            initOptions: function () {
+                options.fromStorageType = 1;
+                options.showToStorage = false;
+            },
+            initData: function () {
+                $scope.newtransfer.stofrom = $scope.newtransfer.stofrom || $scope.storages[0];
 
-            $scope.newtransfer.product = $scope.products[0];
+                /* Find main storage */
+                Storage.findById({ id: 0 })
+                .$promise
+                .then(function (data) {
+                    $scope.newtransfer.stodest = data;
+                });
+            },
+            preAdd: function () {},
+            alert: function (n) {
+                return 'Lisätty' + n.qty + ' ' + n.product.name + ' -> ' + n.stodest.name;
+            }
 
-            $scope.changeProduct();
+        },
+        '/sales': {
+            caption: 'Myynti',
+            submitCaption: 'Lisää',
+            type: 2,
+            initOptions: function () {
+                options.useNegative = true;
+                options.showToStorage = false;
+            },
+            initData: function () {
+                /* Find pseudo sales storage */
+                Storage.findById({ id: 10 })
+                .$promise
+                .then(function (data) {
+                    $scope.newtransfer.stodest = data;
+                });
+                $scope.newtransfer.stofrom = $scope.newtransfer.stofrom || $scope.storages[0];
+            },
+            preAdd: function () {},
+            alert: function (n) {
+                return 'Lisätty myynti ' + n.qty + ' ' + n.product.name + ' ' + n.stofrom.name;
+            }
+
+        },
+        '/inventory': {
+            caption: 'Inventaario',
+            submitCaption: 'Kirjaa',
+            type: 3,
+            initOptions: function () {
+                options.useAbsolute = true;
+                options.showFromStorage = true;
+                options.showToStorage = false;
+                options.showStorageLabels = false;
+            },
+            initData: function () {
+                $scope.newtransfer.stofrom = $scope.newtransfer.stofrom || $scope.storages[0];
+            },
+            preAdd: function () {
+                $scope.newtransfer.stodest = $scope.newtransfer.stofrom;
+            },
+            alert: function (n) {
+                return '' + n.product.name + ' ' + n.stofrom.name + ' määrä ' + n.qty;
+            }
+        }
+    };
+
+    if ($scope.actions[$location.path()]) {
+        $scope.action = $scope.actions[$location.path()];
+    } else {
+        $scope.action = $scope.actions['/transfer'];
+    }
+    $scope.action.initOptions();
+
+    $q.all([
+        Product.find().$promise,
+        Storage.find({filter: {where: { 'type': options.fromStorageType }}}).$promise
+    ]).then(function (data) {
+        $scope.products = data[0];
+        _($scope.products).forEach(function (i) {
+            i.qs = JSON.parse(i.qtys);
         });
+        $scope.newtransfer.product = $scope.products[0];
+        $scope.changeProduct();
 
-    Storage.find()
-        .$promise
-        .then(function (data) {
-            $scope.storages = data;
+        $scope.storages = data[1];
 
-            $scope.newtransfer.stofrom = $scope.storages[0];
-            $scope.newtransfer.stodest = $scope.storages[1];
-        });
+        $scope.action.initData();
+    });
 
     $scope.changeProduct = function () {
         var p = $scope.newtransfer.product;
@@ -75,13 +150,20 @@ angular.module('hkApp.controllers')
     $scope.addTransfer = function () {
         var n = $scope.newtransfer;
 
-        var t = '' + n.qty + ' ' + n.product.name + ' ' + n.stofrom.name + ' -> ' + n.stodest.name;
+        /* Call pre-add method of action. */
+        $scope.action.preAdd();
+
+        var t = $scope.action.alert(n);
 
         var absolute = 0;
         var relative = n.qty;
-        if ($scope.config.useAbsolute) {
+        if (options.useAbsolute) {
             absolute = qty;
             relative = 0;
+        }
+        var factor = 1.0;
+        if (options.useNegative) {
+            factor = -1.0;
         }
 
         Transfer.create({
@@ -89,10 +171,10 @@ angular.module('hkApp.controllers')
             "tostorageid": n.stodest.id,
             "productid": n.product.id,
             "user": 0,
-            "absolute": absolute,
-            "relative": relative,
+            "absolute": factor * absolute,
+            "relative": factor * relative,
             "comment": n.comment,
-            "type": $scope.type
+            "type": $scope.action.type
             //"id": 0,
             //"inserttime": "string",
             //"transfertime": "string"
@@ -109,4 +191,4 @@ angular.module('hkApp.controllers')
 
 
     };
-}]);
+}]);;
