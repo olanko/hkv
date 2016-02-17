ad4aab361214caf10966a7932667bc673898ef7d
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-18 01:18:57 +0200

    jslint cleanups

--- public/js/currentqtysservice.js
+++ public/js/currentqtysservice.js
@@ -12,16 +12,16 @@ angular.module('hkApp.controllers')
                 if (!runningvalues[transfers[i].productid]) {
                     runningvalues[transfers[i].productid] = {};
                 }
-                if (transfers[i].type == 3) {
-                    transfers[i].runningvalue = 1.0 * transfers[i].absolute;
+                if (transfers[i].type === 3) {
+                    transfers[i].runningvalue = +transfers[i].absolute;
 
                     /* If first absolute value or value matches running value, then fine. */
-                    if (!runningvalues[transfers[i].productid].value || runningvalues[transfers[i].productid].value == transfers[i].absolute) {
+                    if (!runningvalues[transfers[i].productid].value || +runningvalues[transfers[i].productid].value === +transfers[i].absolute) {
                         transfers[i].class = 'bg-success';
                     } else {
                         transfers[i].class = 'bg-danger';
                     }
-                    runningvalues[transfers[i].productid].value = 1.0 * transfers[i].absolute;
+                    runningvalues[transfers[i].productid].value = +transfers[i].absolute;
                 } else {
                     if (!runningvalues[transfers[i].productid].value) {
                         runningvalues[transfers[i].productid].value = 0;
@@ -32,22 +32,22 @@ angular.module('hkApp.controllers')
 
                     /* Transfer and delivery */
                     if (transfers[i].type === 0 || transfers[i].type === 1) {
-                        if ((transfers[i].fromstorageid | 0) == storage.id) {
+                        if (+transfers[i].fromstorageid === storage.id) {
                             transfers[i].change += -1.0 * transfers[i].relative;
 
                         }
-                        if (transfers[i].tostorageid == storage.id) {
-                            transfers[i].change += 1.0 * transfers[i].relative;
+                        if (+transfers[i].tostorageid === storage.id) {
+                            transfers[i].change += +transfers[i].relative;
                         }
                     }
 
                     /* Sales */
                     if (transfers[i].type === 2) {
-                        transfers[i].change += 1.0 * transfers[i].relative;
+                        transfers[i].change += +transfers[i].relative;
                     }
 
-                    runningvalues[transfers[i].productid].value += 1.0 * transfers[i].change;
-                    transfers[i].runningvalue = 1.0 * runningvalues[transfers[i].productid].value;
+                    runningvalues[transfers[i].productid].value += +transfers[i].change;
+                    transfers[i].runningvalue = +runningvalues[transfers[i].productid].value;
                 }
                 if (i > 0) {
                     transfers[i].diff = transfers[i].runningvalue - transfers[i - 1].runningvalue;
