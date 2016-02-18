cac67cd2b5df509380b90ff34110f1432765de29
Olli Korhonen <olli.korhonen@iki.fi>
2016-02-18 18:13:42 +0200

    navbar username working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,11 @@
 /*global angular */
 angular.module('hkApp.controllers', [])
 
-.controller('MainCtrl', ['$scope', 'Storage', 'User', function ($scope, Storage, User) {
+.controller('MainCtrl', ['$scope', '$rootScope', 'Storage', 'User', function ($scope, $rootScope, Storage, User) {
     'use strict';
+
+    $scope.user = {};
+
     Storage.find()
     .$promise
     .then(function (data) {
@@ -10,12 +13,31 @@ angular.module('hkApp.controllers', [])
     });
 
     $scope.User = User;
-    $scope.user = User.getCurrent();
-}])
-.controller('LoginCtrl', ['$scope', '$location', 'User', function ($scope, $location, User) {
-    'use strict';
 
-    console.log($location);
+    if (User.isAuthenticated()) {
+        $scope.user = $rootScope.user;
+    }
+
+    $rootScope.$watch('user', function (newValue, oldValue) {
+        if (User.isAuthenticated()) {
+            $scope.user = User.getCurrent();
+        } else {
+            $scope.user = {};
+        }
+    });
+
+    $scope.logout = function () {
+        User.logout()
+        .$promise
+        .then(function (data) {
+            $rootScope.user = {};
+        }).catch(function (err) {
+            console.log(err);
+        });
+    };
+}])
+.controller('LoginCtrl', ['$scope', '$rootScope', '$location', 'User', function ($scope, $rootScope, $location, User) {
+    'use strict';
 
     $scope.user = {};
     $scope.user.login = function () {
@@ -25,6 +47,8 @@ angular.module('hkApp.controllers', [])
         User.login(user)
         .$promise
         .then(function (token) {
+            /* Tell watcher to update navbar user name. */
+            $rootScope.user = token.user;
             var next = $location.nextAfterLogin || '/';
             $location.nextAfterLogin = null;
             $location.path(next);
