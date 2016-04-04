9e060a2ebf5cb6e3c177b32a5f1ffc4d1cb9d409
Olli Korhonen <olli.korhonen@iki.fi>
2016-04-04 13:39:51 +0300

    query helpers

---
+++ public/js/queryshortcuts.js
@@ -0,0 +1,34 @@
+var QS = (function () {
+    function transfersByType (Transfer, queryparams, type) {
+        var begindate = '2016-03-20';
+        queryparams = queryparams || {};
+
+        if (queryparams['begindate']) {
+            begindate = queryparams['begindate'];
+        }
+
+        return Transfer.find({filter: {
+            where: {
+                'transfertime': {'gte': begindate},
+                type: type
+            },
+        }});
+    }
+    var queries = {
+        transfers: function (Transfer, queryparams) {
+            return transfersByType(Transfer, queryparams, 0);
+        },
+        deliveries: function (Transfer, queryparams) {
+            return transfersByType(Transfer, queryparams, 1);
+        },
+        sales: function (Transfer, queryparams) {
+            return transfersByType(Transfer, queryparams, 2);
+        },
+        inventories: function (Transfer, queryparams) {
+            return transfersByType(Transfer, queryparams, 3);
+        }
+    };
+    return queries;
+}());
+
+// , {where: {'transfertime': {gt: '2016-03-20'}}}
