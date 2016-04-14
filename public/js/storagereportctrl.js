dd9e7cccde923107b989ec98f5b208421ff3d9b6
Olli Korhonen <olli.korhonen@iki.fi>
2016-04-14 13:01:07 +0300

    data fetch

--- public/js/storagereportctrl.js
+++ public/js/storagereportctrl.js
@@ -39,12 +39,13 @@ angular.module('hkApp.controllers')
                 inventories = {},
                 waste = {},
                 tosto = {},
-                fromsto = {};
+                fromsto = {},
+                reportdata = {};
 
             var storageid = $scope.filters.storage.id;
             queryparams = {
-                begindate: '2016-03-20',
-                enddate: '2016-04-28',
+                begindate: '2016-04-02',
+                enddate: '2016-04-11',
                 storageid: storageid
             };
 
@@ -56,8 +57,49 @@ angular.module('hkApp.controllers')
                 sales = _.filter(transfers, {'type': 2, 'fromstorageid': '' + storageid });
                 inventories = _.filter(transfers, {'type': 3, tostorageid: '' + storageid });
                 waste = _.filter(transfers, {type: 0, tostorageid: '' + WASTESTOID });
+
+                tosto = groupByProduct(tosto);
+                fromsto = groupByProduct(fromsto);
+                deliveries = groupByProduct(deliveries);
+                sales = groupByProduct(sales);
+                waste = groupByProduct(waste);
+
+                _.forEach($scope.products, function (product) {
+                    var pid = product.id;
+                    console.log(inventor);
+
+                    reportdata[pid] = {
+                        name: product.name,
+                        transfers: soz(tosto, pid) - (soz(fromsto, pid) - soz(waste, pid)),
+                        deliveries: soz(deliveries, pid),
+                        sales: soz(sales, pid),
+                        waste: soz(waste, pid),
+                        startvalue: _.find(inventories, {'productid': pid}),
+                        endvalue: _.findLast(inventories, {'productid': pid}).absolute
+                    }
+                });
+
+                $scope.reportdata = reportdata;
+                console.log(reportdata);
             });
         };
+
+        function soz(list, index) {
+            return list[index] ? +list[index].sum : 0;
+        }
+        function groupByProduct(list) {
+            return _(list)
+            .groupBy('productid')
+            .transform(function (result, current, key) {
+                result[key] = {
+                    productid: current[0].productid,
+                    sum: _.reduce(current, function (sum, item) {
+                        return +sum + +item.relative;
+                    }, 0.0)
+                };
+            }, [])
+            .value();
+        }
 
         $scope.$watchCollection('filters.storage', function (newSto, oldSto, $scope) {
             if (!newSto) {
