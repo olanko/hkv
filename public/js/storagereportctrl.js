41ba7841f74eacaaa8f827952ac86707b38bd625
Olli Korhonen <olli.korhonen@iki.fi>
2016-04-14 13:37:46 +0300

    show data

--- public/js/storagereportctrl.js
+++ public/js/storagereportctrl.js
@@ -1,8 +1,11 @@
-/*global angular */
+/*globals angular,_ */
+/*jslint nomen: true */
+/*jshint nomen: true */
+
 angular.module('hkApp.controllers')
 .controller('StorageReportCtrl',
-    ['$scope', '$routeParams', '$q', 'Storage', 'Product', 'Transfer', 'CurrentQtysService',
-    function  ($scope, $routeParams, $q, Storage, Product, Transfer, CurrentQtysService) {
+    ['$scope', '$routeParams', 'Storage', 'Product', 'Transfer',
+    function  ($scope, $routeParams, Storage, Product, Transfer) {
         'use strict';
         $scope.types = ['Siirto', 'Toimitus', 'Myynti', 'Inventaario'];
 
@@ -16,17 +19,31 @@ angular.module('hkApp.controllers')
 
         $scope.products = Product.find();
 
-        var queryparams = {
-            begindate: '2016-03-20',
-            enddate: '2016-03-27'
-        };
-
         /* Current storage */
         Storage.findById({'id': $routeParams.storageid})
             .$promise
             .then(function (data) {
                 $scope.filters.storage = data;
             });
+
+
+        function soz(list, index) {
+            return list[index] ? +list[index].sum : 0;
+        }
+
+        function groupByProduct(list) {
+            return _(list)
+            .groupBy('productid')
+            .transform(function (result, current, key) {
+                result[key] = {
+                    productid: current[0].productid,
+                    sum: _.reduce(current, function (sum, item) {
+                        return +sum + +item.relative;
+                    }, 0.0)
+                };
+            }, [])
+            .value();
+        }
 
         $scope.reportdata = {};
 
@@ -42,21 +59,21 @@ angular.module('hkApp.controllers')
                 fromsto = {},
                 reportdata = {};
 
-            var storageid = $scope.filters.storage.id;
-            queryparams = {
-                begindate: '2016-04-02',
-                enddate: '2016-04-11',
+            var storageid = $scope.filters.storage.id.toString();
+            var queryparams = {
+                begindate: '2016-04-06T11:15:00.000Z',
+                enddate: '2016-04-11T07:10:00.000Z',
                 storageid: storageid
             };
 
             Transfer.allByTime(queryparams, function (results) {
                 transfers = results.data;
-                tosto = _.filter(transfers, {'type': 0, tostorageid: '' + storageid });
-                fromsto = _.filter(transfers, {'type': 0, fromstorageid: '' + storageid });
-                deliveries = _.filter(transfers, {'type': 1, tostorageid: '' + storageid });
-                sales = _.filter(transfers, {'type': 2, 'fromstorageid': '' + storageid });
-                inventories = _.filter(transfers, {'type': 3, tostorageid: '' + storageid });
-                waste = _.filter(transfers, {type: 0, tostorageid: '' + WASTESTOID });
+                tosto = _.filter(transfers, {'type': 0, tostorageid: storageid });
+                fromsto = _.filter(transfers, {'type': 0, fromstorageid: storageid });
+                deliveries = _.filter(transfers, {'type': 1, tostorageid: storageid });
+                sales = _.filter(transfers, {'type': 2, 'fromstorageid': storageid });
+                inventories = _.filter(transfers, {'type': 3, tostorageid: storageid });
+                waste = _.filter(transfers, {type: 0, tostorageid: WASTESTOID.toString() });
 
                 tosto = groupByProduct(tosto);
                 fromsto = groupByProduct(fromsto);
@@ -65,8 +82,11 @@ angular.module('hkApp.controllers')
                 waste = groupByProduct(waste);
 
                 _.forEach($scope.products, function (product) {
-                    var pid = product.id;
-                    console.log(inventor);
+                    var pid = product.id.toString(),
+                        first = _.find(inventories, {'productid': pid}),
+                        last = _.findLast(inventories, {'productid': pid}),
+                        r;
+                    console.log(first);
 
                     reportdata[pid] = {
                         name: product.name,
@@ -74,8 +94,18 @@ angular.module('hkApp.controllers')
                         deliveries: soz(deliveries, pid),
                         sales: soz(sales, pid),
                         waste: soz(waste, pid),
-                        startvalue: _.find(inventories, {'productid': pid}),
-                        endvalue: _.findLast(inventories, {'productid': pid}).absolute
+                        startvalue: first ? +first.absolute : 0,
+                        endvalue: last ? +last.absolute : 0
+                    }
+                    r = reportdata[pid];
+
+                    r.expected = r.startvalue + r.transfers + r.deliveries + r.sales - r.waste
+
+                    if (r.expected !== 0 && r.expected == r.endvalue) {
+                        r.class = 'bg-success';
+                    }
+                    if (r.expected !== 0 && r.expected !== r.endvalue) {
+                        r.class = 'bg-danger';
                     }
                 });
 
@@ -83,23 +113,6 @@ angular.module('hkApp.controllers')
                 console.log(reportdata);
             });
         };
-
-        function soz(list, index) {
-            return list[index] ? +list[index].sum : 0;
-        }
-        function groupByProduct(list) {
-            return _(list)
-            .groupBy('productid')
-            .transform(function (result, current, key) {
-                result[key] = {
-                    productid: current[0].productid,
-                    sum: _.reduce(current, function (sum, item) {
-                        return +sum + +item.relative;
-                    }, 0.0)
-                };
-            }, [])
-            .value();
-        }
 
         $scope.$watchCollection('filters.storage', function (newSto, oldSto, $scope) {
             if (!newSto) {
