da0d4665b14d4931ba42303a99ac51ba278df355
Olli Korhonen <olli.korhonen@flashnode.com>
2017-08-14 12:05:55 +0300

    Backup script changes to make it work

--- scripts/makebackup.sh
+++ scripts/makebackup.sh
@@ -8,8 +8,8 @@ if psql -lqt | cut -d \| -f 1 | grep -qw $prefix-$d; then
     echo "Database $prefix-$d already exists. You might want to do something else instead. Grab a coffee or something."
 else
     echo "Creating $prefix-$d..."
-    createdb hkv-backup-$d
+    #createdb hkv-backup-$d
     echo "Pulling data from heroku DATABASE to $prefix-$d..."
-    heroku pg:pull DATABASE postgres://postgres:postgres@192.168.0.3/$prefix-$d
+    heroku pg:pull DATABASE postgres://postgres:postgres@$POSTGRES_IP/$prefix-$d
     echo "Done."
 fi
